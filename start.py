d3c063f2a9339e8a08ce0b450cd549d50fd3a571
Bryce Bjork <brycedbjork@gmail.com>
2019-09-19 15:36:50 -0400

    joystick button?

--- start.py
+++ start.py
@@ -2,33 +2,34 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-# joystick
-# 2 is joystick button: 0 when pressed, 1 when not
-GPIO.setup([2, 3, 4], GPIO.IN)
-if (GPIO.input(2)):
-    joystickButton = 0
-else:
-    joystickButton = 1
-
-if (GPIO.input(3)):
-    joystickUp = 0
-else:
-    joystickUp = 1
-
-if (GPIO.input(4)):
-    joystickLeft = 0
-else:
-    joystickLeft = 1
-
-# button
-GPIO.setup(24, GPIO.IN)
-button = GPIO.input(24)
-
-# switch
-GPIO.setup(25, GPIO.IN)
-switch = GPIO.input(25)
-
 while True:
+
+    # joystick
+    # 2 is joystick button: 0 when pressed, 1 when not
+    GPIO.setup([2, 3, 4], GPIO.IN)
+    if (GPIO.input(2)):
+        joystickButton = 0
+    else:
+        joystickButton = 1
+
+    if (GPIO.input(3)):
+        joystickUp = 0
+    else:
+        joystickUp = 1
+
+    if (GPIO.input(4)):
+        joystickLeft = 0
+    else:
+        joystickLeft = 1
+
+    # button
+    GPIO.setup(24, GPIO.IN)
+    button = GPIO.input(24)
+
+    # switch
+    GPIO.setup(25, GPIO.IN)
+    switch = GPIO.input(25)
+
     if joystickButton:
         print("play a song!!!")
 
