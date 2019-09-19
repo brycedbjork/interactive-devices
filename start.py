d74b964937c73dd3c4da3a19ea9f47251adbb28f
Bryce Bjork <brycedbjork@gmail.com>
2019-09-19 15:30:13 -0400

    better printing

--- start.py
+++ start.py
@@ -3,15 +3,33 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 # joystick
+# 2 is joystick button: 0 when pressed, 1 when not
 GPIO.setup([2, 3, 4], GPIO.IN)
-print("joystick2 "+str(GPIO.input(2)))
-print("joystick3 "+str(GPIO.input(3)))
-print("joystick4 "+str(GPIO.input(4)))
+if (GPIO.input(2)):
+    joystickButton = 0
+else:
+    joystickButton = 1
+
+if (GPIO.input(3)):
+    joystickUp = 0
+else:
+    joystickUp = 1
+
+if (GPIO.input(4)):
+    joystickLeft = 0
+else:
+    joystickLeft = 1
+
+print("joystick button "+str(joystickButton))
+print("joystick up "+str(joystickUp))
+print("joystick left "+str(joystickLeft))
 
 # button
 GPIO.setup(24, GPIO.IN)
-print("button "+str(GPIO.input(24)))
+button = GPIO.input(24)
+print("button "+str(button))
 
 # switch
 GPIO.setup(25, GPIO.IN)
-print("switch "+str(GPIO.input(25)))
+switch = GPIO.input(25)
+print("switch "+str(switch))
