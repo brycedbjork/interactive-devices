a58a0609f4635ac03d17ade8e2d1377bc09e5866
Bryce Bjork <brycedbjork@gmail.com>
2019-09-19 15:20:23 -0400

    joystick

--- start.py
+++ start.py
@@ -4,11 +4,14 @@ GPIO.setmode(GPIO.BCM)
 
 # joystick
 GPIO.setup([2, 3, 4], GPIO.IN)
+print("joystick2 "+str(GPIO.input(2)))
+print("joystick3 "+str(GPIO.input(3)))
+print("joystick4 "+str(GPIO.input(4)))
 
 # button
 GPIO.setup(24, GPIO.IN)
-print("button"+str(GPIO.input(24)))
+print("button "+str(GPIO.input(24)))
 
 # switch
 GPIO.setup(25, GPIO.IN)
-print("switch"+str(GPIO.input(25)))
+print("switch "+str(GPIO.input(25)))
